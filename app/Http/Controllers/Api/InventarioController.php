e392a0bbd425dc09f34741ee6e310a18d667f1cd
Alan Flores <174575232+alan21fg@users.noreply.github.com>
2024-10-22 18:59:01 -0600

    feat: :rocket: Modulo de inventario

---
+++ app/Http/Controllers/Api/InventarioController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Inventario;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreInventarioRequest;
+
+class InventarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $inventarios = Inventario::with('producto')->get();
+        return response()->json($inventarios, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInventarioRequest $request)
+    {
+        $inventario = Inventario::create($request->validated());
+        return response()->json($inventario, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $inventario = Inventario::with('producto')->findOrFail($id);
+        return response()->json($inventario, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreInventarioRequest $request, string $id)
+    {
+        $inventario = Inventario::create($request->validated());
+
+        $inventario = Inventario::findOrFail($id);
+        $inventario->update($request->all());
+
+        return response()->json($inventario, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $inventario = Inventario::findOrFail($id);
+        $inventario->delete();
+
+        return response()->json(null, 204);
+    }
+
+}

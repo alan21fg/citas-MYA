3a63251e28b7ef6a0fa0c5238b6b027238127751
alan21fg <floresguzmanalan@gmail.com>
2024-11-06 17:51:36 -0600

    fix: :bug: cambios ligeros en las configuraciones de seguridad

--- routes/api.php
+++ routes/api.php
@@ -15,20 +15,34 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 // Ruta pública para el inicio de sesión
 Route::post('login', [AuthController::class, 'login']);
+Route::apiResource('inventarios', InventarioController::class);
+Route::apiResource('productos', ProductoController::class);
+Route::apiResource('servicios', ServicioController::class);
+Route::apiResource('citas', CitaController::class);
+Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('empleados', EmpleadoController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('roles', RolController::class);
+
+Route::get('reportes/estadisticas-mes', [ReporteController::class, 'reporteEstadisticasMes']);
+Route::get('reportes/citas-proximas', [ReporteController::class, 'reporteCitasProximas']);
+Route::get('reportes/inventario-critico', [ReporteController::class, 'reporteInventarioCritico']);
+Route::get('reportes/citas-por-mes', [ReporteController::class, 'reporteCitasPorMes']);
+
 
 // Rutas protegidas con auth:sanctum
 Route::middleware('auth:sanctum', EnsureFrontendRequestsAreStateful::class)->group(function () {
-    Route::apiResource('inventarios', InventarioController::class);
-    Route::apiResource('productos', ProductoController::class);
-    Route::apiResource('servicios', ServicioController::class);
-    Route::apiResource('citas', CitaController::class);
-    Route::apiResource('clientes', ClienteController::class);
-    Route::apiResource('empleados', EmpleadoController::class);
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('roles', RolController::class);
+    // Route::apiResource('inventarios', InventarioController::class);
+    // Route::apiResource('productos', ProductoController::class);
+    // Route::apiResource('servicios', ServicioController::class);
+    // Route::apiResource('citas', CitaController::class);
+    // Route::apiResource('clientes', ClienteController::class);
+    // Route::apiResource('empleados', EmpleadoController::class);
+    // Route::apiResource('users', UserController::class);
+    // Route::apiResource('roles', RolController::class);
 
-    Route::get('reportes/estadisticas-mes', [ReporteController::class, 'reporteEstadisticasMes']);
-    Route::get('reportes/citas-proximas', [ReporteController::class, 'reporteCitasProximas']);
-    Route::get('reportes/inventario-critico', [ReporteController::class, 'reporteInventarioCritico']);
-    Route::get('reportes/citas-por-mes', [ReporteController::class, 'reporteCitasPorMes']);
+    // Route::get('reportes/estadisticas-mes', [ReporteController::class, 'reporteEstadisticasMes']);
+    // Route::get('reportes/citas-proximas', [ReporteController::class, 'reporteCitasProximas']);
+    // Route::get('reportes/inventario-critico', [ReporteController::class, 'reporteInventarioCritico']);
+    // Route::get('reportes/citas-por-mes', [ReporteController::class, 'reporteCitasPorMes']);
 });
